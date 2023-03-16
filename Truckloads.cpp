4d2407d9221cfdc911994b39aa5d95d37927c387
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-03-16 20:26:27 +1030

    Commented Truckloads

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 int Truckloads::numTrucks(int numCrates, int loadSize)
 {
+    // if number of crates is less than 0 or the max size that can fit in a truck is less than 0, return -1
     if (numCrates < 0 || loadSize < 0)
         return -1;
-
+    // if number of creates is 0, then we need 0 trucks
     if (numCrates == 0 || loadSize == 0)
         return 0;
 
@@ -15,6 +16,6 @@ int Truckloads::numTrucks(int numCrates, int loadSize)
     {
         return 1;
     }
-        
+    // refer to the oneNote diagram 
     return (numTrucks(numCrates/2 + numCrates % 2, loadSize) + numTrucks(numCrates/2, loadSize));
 }
