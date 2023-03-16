6f71a62d9335f98fc0a4d316b4c660dfb1748795
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-03-16 21:53:27 +1030

    reverse digit and string

---
+++ Reverser.cpp
@@ -0,0 +1,43 @@
+#include "Reverser.h"
+
+#include <string>
+
+// helper function to enable tail recursion 
+int helper_numbers(int numbers, int accum)
+{
+    accum = accum*10 + numbers % 10;
+    // base case
+    if (numbers < 10)
+    {
+        return accum;
+    }
+    return helper_numbers(numbers/10, accum);
+}
+
+int Reverser::reverseDigit(int numbers)
+{
+    if (numbers < 0)
+    {
+        return -1;
+    }
+    return (numbers,0);
+}
+
+// helper function 
+std::string helper_string (std::string characters, std::string accum, int index)
+{
+    // base case
+    if (index < 0)
+    {
+        return accum;
+    }
+    // using at becuase it will help not get index out of bound 
+    accum = accum + letters.at(index);
+
+    return helper_string(characters, accum, --index);
+}
+
+std::string reverseString(std::string characters)
+{
+    return helper_string(characters, "",(characters.length()-1))
+}
---
+++ Reverser.h
@@ -0,0 +1,15 @@
+// Implement a class called Reverser which has two recursive functions: reverseDigit and reverseString
+#ifndef REVERSER_H
+#define REVERSER_H
+
+#include <string>
+
+class Reverser()
+{
+    public:
+    int reverseDigit(int numbers);
+    std::string reverseString(std::string characters);
+    
+}
+
+#endif
